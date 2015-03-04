9d9865a01778d3728fdd2a0737daea8b1e2a5f98
Gerry Leonard <leonardger@gmail.com>
2015-03-04 13:16:49 +0000

    clear input field

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
 
 $(function(){
+
+
+
   $('#search-form').submit(function(event){ // when the user clicks submit....
     event.preventDefault(); // prevent the form from submitting
     var searchTerm = $('#query').val(); // take whatever is in the search field and put it in searchTerm
     getRequest(searchTerm); //call the function "getRequest" and pass it the search term
+    
   });
 });
 
@@ -27,11 +31,16 @@ function getRequest(searchTerm){
 }
 
 function showResults(results){ //shows the results to the user
+  $('#query').val('');
+  $('#search-results').empty();
   var html = ""; // variable to hold the html
   $.each(results, function(index,items){ //for each of the results
 //debugger;
-    //html += '<p>' + items.snippet.thumbnails.default.url + '</p>'; // create a new paragraph with the title
-    html += '<li><iframe src="https://www.youtube.com/embed/"' + items.id.videoId + '</iframe></li>';
+    html += '<p>' + items.snippet.title + '</p>'; // create a new paragraph with the title
+    html+='<li><iframe src="https://www.youtube.com/embed/' + items.id.videoId + '"></iframe></li>';
+
+
+
     //html += '<img src=' + items.snippet.thumbnails.default.url + '>'; 
    // html += '</a>';
 
@@ -43,3 +52,4 @@ function showResults(results){ //shows the results to the user
 
 
 });
+
